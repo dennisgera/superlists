39b463953ea92363ae9ccea428478d3f19db6fd6
Dennis Gera <geradennis@gmail.com>
2024-02-18 21:02:51 -0300

    feat: url mapping

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from lists import views
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path(r'', views.home_page, name='home'),
+
 ]
