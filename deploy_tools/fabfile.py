e56dbec3e5412c52c2cca6b7cb92766ac17c8dcd
Dennis Gera <geradennis@gmail.com>
2024-02-29 06:25:36 -0300

    feat: fabfile edit

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -14,9 +14,11 @@ def deploy():
     _update_static_files(source_folder)
     _update_database(source_folder)
 
+
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
         run(f'mkdir -p {site_folder}/{subfolder}')
+
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
