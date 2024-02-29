46886ce49154080ccc4d007b03be745ab5fd9d54
Dennis Gera <geradennis@gmail.com>
2024-02-29 08:27:05 -0300

    moved fnc tests

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,11 @@
+from unittest import skip
+
+from functional_tests.base import FunctionalTest
+
+
+class ItemValidationTest(FunctionalTest):
+    
+    def test_cannot_add_empty_list(self):
+        # Edith goes to the home page
+        self.fail('write me!')
+        ...
