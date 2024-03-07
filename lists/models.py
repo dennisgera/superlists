2ff95ea07aac5b2c9bf12e46de7d149111009b50
Dennis Gera <geradennis@gmail.com>
2024-03-07 06:31:24 -0300

    feat: add test for uniqueness

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,17 @@
+from typing import Any
 from django.db import models
 from django.urls import reverse
 
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey('List', default=None, on_delete=models.CASCADE)
+
+    def __str__(self) -> Any:
+        return self.text
+
+    class Meta:
+        unique_together = ('list', 'text')
+        ordering = ('id',)
 
 class List(models.Model):
 
